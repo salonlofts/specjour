35a0eb666754fc023dbea06d0ee47d703ce00878
Scott Dunifon <scott.dunifon@salonlofts.com>
2019-09-10 14:54:04 -0400

    ported code  for runner that runs rspec with the description of the example

--- lib/specjour/rspec/runner.rb
+++ lib/specjour/rspec/runner.rb
@@ -2,13 +2,29 @@ module Specjour::RSpec::Runner
   ::RSpec.configuration.backtrace_exclusion_patterns << %r(lib/specjour/)
 
   def self.run(spec, output)
-    args = ['--format=Specjour::RSpec::SpecjourFormatter', spec]
-    ::RSpec::Core::Runner.run args, $stderr, output
-  ensure
-    ::RSpec.configuration.filter_manager = ::RSpec::Core::FilterManager.new
-    ::RSpec.world.filtered_examples.clear
-    ::RSpec.world.inclusion_filter.clear
-    ::RSpec.world.exclusion_filter.clear
-    ::RSpec.world.send(:instance_variable_set, :@line_numbers, nil)
+    Specjour.logger.debug  '------------------in Specjour::Runner.run---------------'
+    Specjour.logger.debug  spec
+
+    if spec.include?('--description')
+      location = spec.split('--description')[0]
+      full_description = spec.split('--description')[1]
+      args = ['--format=Specjour::RSpec::WorkerFormatter',location.gsub(/:\d*/,''),  "-e#{full_description}"]
+    else
+      args = ['--format=Specjour::RSpec::DistributedFormatter',spec]
+    end
+    Specjour.logger.debug "----rspec args=#{args}"
+
+    Specjour.logger.debug 'before rspec core run'
+    ::RSpec::Core::Runner.run args, $stderr, output 
+    Specjour.logger.debug  'after  rspec core run'
+
+
+    ensure
+      Specjour.logger.debug 'in runner ensure'
+      ::RSpec.configuration.filter_manager = ::RSpec::Core::FilterManager.new
+      ::RSpec.world.filtered_examples.clear
+      ::RSpec.world.inclusion_filter.clear
+      ::RSpec.world.exclusion_filter.clear
+      ::RSpec.world.send(:instance_variable_set, :@line_numbers, nil)
   end
 end
